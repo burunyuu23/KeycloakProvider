4d81b3736a9093a8d309303795fc1614d7df3aa7
danilkk <ezzfvkoko@gmail.com>
2023-08-26 22:20:54 +0300

    chore: add AppConfigOrigin

---
+++ src/main/java/com/example/keycloakprovider/util/AppConfigOrigin.java
@@ -0,0 +1,12 @@
+package com.example.keycloakprovider.util;
+
+public class AppConfigOrigin {
+    public static final String SERVER_URL = "";
+    public static final String REALM_NAME = "";
+    public static final String REALM_MASTER = "";
+    public static final String ADMIN_CLI = "";
+    public static final String USER_CONSOLE = "";
+    public static final String PASSWORD_CONSOLE = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String CLIENT_ID = "";
+}
