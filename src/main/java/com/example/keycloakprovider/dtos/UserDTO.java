ebc53d3feda2f6d970f79ef2abf7e2b11cc8738a
danilkk <ezzfvkoko@gmail.com>
2023-08-27 14:39:38 +0300

    refactor: userDTO jsonProperty

--- src/main/java/com/example/keycloakprovider/dtos/UserDTO.java
+++ src/main/java/com/example/keycloakprovider/dtos/UserDTO.java
@@ -1,5 +1,7 @@
 package com.example.keycloakprovider.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.annotations.SerializedName;
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,7 +16,9 @@ public class UserDTO implements Serializable{
     String username;
     String email;
     String password;
+    @JsonProperty("first_name")
     String firstName;
+    @JsonProperty("last_name")
     String lastName;
     LocalDate birthdate;
     Set<String> roles;
