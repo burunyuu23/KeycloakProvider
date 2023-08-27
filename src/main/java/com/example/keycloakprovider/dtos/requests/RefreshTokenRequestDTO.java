1a0b7480d64f9b859d6f42bbbe8b2f6a8f898270
danilkk <ezzfvkoko@gmail.com>
2023-08-27 22:41:05 +0300

    fix: @JsonProperty for refresh_token

--- src/main/java/com/example/keycloakprovider/dtos/requests/RefreshTokenRequestDTO.java
+++ src/main/java/com/example/keycloakprovider/dtos/requests/RefreshTokenRequestDTO.java
@@ -1,8 +1,10 @@
 package com.example.keycloakprovider.dtos.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class RefreshTokenRequestDTO {
+    @JsonProperty("refresh_token")
     private String refreshToken;
 }
