6e88e386ea8cfa800865594666288320f5d90ced
danilkk <ezzfvkoko@gmail.com>
2023-08-28 18:30:52 +0300

    fix: jwt Decoder

--- src/main/java/com/example/keycloakprovider/security/SecurityConfig.java
+++ src/main/java/com/example/keycloakprovider/security/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,8 +24,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkUri;
     @Value("${frontend.url}")
     private String frontendUrl;
 
@@ -28,24 +33,46 @@ public class SecurityConfig {
     private final JwtAuthenticationConverter jwtAuthenticationConverter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors((httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
                         .configurationSource(corsConfigurationSource)));
 
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(http -> {
-                    http.requestMatchers("/user/create", "/auth/**").permitAll();
-                    http.anyRequest().authenticated();
-                })
-                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+        http
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/user/**", "/auth/*").permitAll()
+                                .anyRequest()
+                                .authenticated());
+
+        http
+                .oauth2ResourceServer((resourceServerConfigurer) ->
+                        resourceServerConfigurer.jwt((jwtConfigurer) -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter))
+                );
+
+        http
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
     }
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // return your JWTdecoder
+        // Use NimbusJwtDecoder to decode the JWT
+        OAuth2TokenValidator<Jwt> jwtValidator = JwtValidators.createDefault();
 
+        // Создайте экземпляр NimbusJwtDecoder с указанием URI набора JWK
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkUri).build();
+        jwtDecoder.setJwtValidator(jwtValidator);
 
+        // Добавьте валидатор для проверки наличия ролей в токене
+//        jwtValidator = new DelegatingOAuth2TokenValidator<>(jwtValidator, customJwtValidator);
+//        jwtDecoder.setJwtValidator(jwtValidator);
+
+        return jwtDecoder;
+    }
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
